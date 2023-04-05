16e3be479d59c7243ad6c512831fc552a34b78b8
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-05 15:07:57 +0900

    {Feat} jwt

---
+++ src/main/java/project/gaori/server/domain/auth/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,16 @@
+package project.gaori.server.domain.auth.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String name;
+    private String userId;
+    private boolean isLogin;
+    private Long roleId;
+}
