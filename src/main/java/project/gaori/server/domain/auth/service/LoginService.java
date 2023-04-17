163482d6a30b3f23a2adde364a3e0e0a33fb5aa6
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-17 14:06:23 +0900

    {Doc} import 정리

--- src/main/java/project/gaori/server/domain/auth/service/LoginService.java
+++ src/main/java/project/gaori/server/domain/auth/service/LoginService.java
@@ -1,12 +1,12 @@
 package project.gaori.server.domain.auth.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import project.gaori.server.domain.auth.presentation.dto.request.LoginRequest;
 import project.gaori.server.domain.auth.presentation.dto.response.TokenResponse;
 import project.gaori.server.domain.user.entity.User;
 import project.gaori.server.domain.user.facade.UserFacade;
 import project.gaori.server.global.security.jwt.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +21,6 @@ public class LoginService {
         return TokenResponse.builder()
                 .accessToken(jwtTokenProvider.generateAccessToken(user.getEmail()))
                 .refreshToken(jwtTokenProvider.generateRefreshToken(user.getEmail()))
-                .isLogin(!(user.getNickname() == null || user.getNickname().equals("")))
                 .build();
     }
 
