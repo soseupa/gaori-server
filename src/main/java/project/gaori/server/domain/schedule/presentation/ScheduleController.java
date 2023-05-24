47ec5de5821802e4f27ca3418213ba53772aced4
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-24 20:37:38 +0900

    {Add} AddUserToScheduleService

--- src/main/java/project/gaori/server/domain/schedule/presentation/ScheduleController.java
+++ src/main/java/project/gaori/server/domain/schedule/presentation/ScheduleController.java
@@ -1,11 +1,14 @@
 package project.gaori.server.domain.schedule.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.gaori.server.domain.schedule.presentation.dto.request.CreateScheduleRequest;
+import project.gaori.server.domain.schedule.service.AddUserToScheduleService;
 import project.gaori.server.domain.schedule.service.CreateScheduleService;
 
 @RestController
@@ -13,10 +16,16 @@ import project.gaori.server.domain.schedule.service.CreateScheduleService;
 @RequiredArgsConstructor
 public class ScheduleController {
     private final CreateScheduleService createScheduleService;
+    private final AddUserToScheduleService addUserToScheduleService;
 
     @PostMapping("")
     public void CreateSchedule(@RequestBody CreateScheduleRequest request) {
         createScheduleService.execute(request);
+    }
+
+    @GetMapping("add")
+    public void AddMember(@RequestParam Long id, @RequestParam String email) {
+        addUserToScheduleService.execute(id, email);
     }
 
 }
