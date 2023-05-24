7dfbf35f41c0560a292aba8aaf896b8fbf9012f5
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-24 20:37:12 +0900

    {Feat} scheduleUser를 엔티티로 따로 저장

--- src/main/java/project/gaori/server/domain/schedule/service/CreateScheduleService.java
+++ src/main/java/project/gaori/server/domain/schedule/service/CreateScheduleService.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import project.gaori.server.domain.schedule.entity.Schedule;
 import project.gaori.server.domain.schedule.entity.repository.ScheduleRepository;
 import project.gaori.server.domain.schedule.presentation.dto.request.CreateScheduleRequest;
+import project.gaori.server.domain.schedule.user.entity.ScheduleUser;
+import project.gaori.server.domain.schedule.user.entity.repository.ScheduleUserRepository;
 import project.gaori.server.domain.user.entity.User;
 import project.gaori.server.domain.user.facade.UserFacade;
 
@@ -14,13 +16,17 @@ import project.gaori.server.domain.user.facade.UserFacade;
 public class CreateScheduleService {
     private final UserFacade userFacade;
     private final ScheduleRepository scheduleRepository;
+    private final ScheduleUserRepository scheduleUserRepository;
 
     @Transactional
     public void execute(CreateScheduleRequest request) {
         User user = userFacade.getCurrentUser();
         Schedule schedule = request.toEntity();
-        schedule.addUser(user);
         scheduleRepository.save(schedule);
-
+        ScheduleUser scheduleUser = ScheduleUser.builder()
+                .schedule(schedule)
+                .user(user)
+                .build();
+        scheduleUserRepository.save(scheduleUser);
     }
 }
