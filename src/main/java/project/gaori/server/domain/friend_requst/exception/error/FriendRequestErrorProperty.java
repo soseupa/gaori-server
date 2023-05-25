0d0a2ff6507e62b55d27df11569a2671fd842677
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-26 00:54:32 +0900

    {Add} FriendRequestNotFoundException

--- src/main/java/project/gaori/server/domain/friend_requst/exception/error/FriendRequestErrorProperty.java
+++ src/main/java/project/gaori/server/domain/friend_requst/exception/error/FriendRequestErrorProperty.java
@@ -9,7 +9,8 @@ import project.gaori.server.global.error.exception.ErrorProperty;
 @RequiredArgsConstructor
 public enum FriendRequestErrorProperty implements ErrorProperty {
     FRIEND_SELF_REQUEST_FORBIDDEN(HttpStatus.FORBIDDEN, "자기 자신에게 친구 요청을 보낼 수 없습니다."),
-    FRIEND_REQUEST_FORBIDDEN(HttpStatus.FORBIDDEN, "이미 친구 요쳥을 보냈습니다.");
+    FRIEND_REQUEST_FORBIDDEN(HttpStatus.FORBIDDEN, "이미 친구 요쳥을 보냈습니다."),
+    FRIEND_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "친구 요청을 찾을 수 없습니다.");
     private final HttpStatus status;
     private final String message;
 }
