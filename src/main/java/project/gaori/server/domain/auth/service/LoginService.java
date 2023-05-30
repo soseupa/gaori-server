dfb9ef18e70d75618ef421f46f4e58032cb0ff6a
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-30 17:47:53 +0900

    {Add} 비밀번호 암호화한거 비교

--- src/main/java/project/gaori/server/domain/auth/service/LoginService.java
+++ src/main/java/project/gaori/server/domain/auth/service/LoginService.java
@@ -1,10 +1,12 @@
 package project.gaori.server.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import project.gaori.server.domain.auth.presentation.dto.request.LoginRequest;
 import project.gaori.server.domain.auth.presentation.dto.response.TokenResponse;
 import project.gaori.server.domain.user.entity.User;
+import project.gaori.server.domain.user.exception.PasswordMismatchException;
 import project.gaori.server.domain.user.facade.UserFacade;
 import project.gaori.server.global.security.jwt.JwtTokenProvider;
 
@@ -14,9 +16,11 @@ public class LoginService {
 
     private final UserFacade userFacade;
     private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
     public TokenResponse execute(LoginRequest request) {
         User user = userFacade.findUserByEmail(request.getEmail());
+        checkPassword(request.getPassword(), user.getPassword());
 
         return TokenResponse.builder()
                 .accessToken(jwtTokenProvider.generateAccessToken(user.getEmail()))
@@ -24,6 +28,13 @@ public class LoginService {
                 .email(user.getEmail())
                 .name(user.getNickname())
                 .build();
+
+    }
+
+    private void checkPassword(String actual, String expected) {
+        if (!passwordEncoder.matches(actual, expected)) {
+            throw PasswordMismatchException.EXCEPTION;
+        }
     }
 
 }
