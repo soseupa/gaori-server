5f1bb839d35e769e8cd2641c3815197a5021f713
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-30 17:15:38 +0900

    {Feat} friendship과 user fetch join 사용해서 select

--- src/main/java/project/gaori/server/domain/user/entity/repository/UserRepository.java
+++ src/main/java/project/gaori/server/domain/user/entity/repository/UserRepository.java
@@ -1,14 +1,22 @@
 package project.gaori.server.domain.user.entity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import project.gaori.server.domain.user.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByNickname(String nickname);
 
     boolean existsUserByEmail(String email);
 
     Optional<User> findUserByEmail(String email);
+
+    @Query("select u from User u join fetch u.friendList f where f.user=:user")
+    List<User> findAllByFriendships(User user);
+
 }
