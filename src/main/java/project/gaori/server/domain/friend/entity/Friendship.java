a11c3eb27c55fe0e72c3d47da84c27517e847b53
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-30 15:48:47 +0900

    {Add} Friendship Entity

---
+++ src/main/java/project/gaori/server/domain/friend/entity/Friendship.java
@@ -0,0 +1,38 @@
+package project.gaori.server.domain.friend.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.gaori.server.domain.user.entity.User;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "tbl_friends")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id")
+    private User friend;
+
+    @Builder
+    public Friendship(User user, User friend) {
+        this.user = user;
+        this.friend = friend;
+    }
+}
