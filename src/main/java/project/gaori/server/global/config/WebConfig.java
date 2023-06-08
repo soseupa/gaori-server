e0ab286bd9c147b837359aeb4ea70d9ba92ce153
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-08 22:18:20 +0900

    {Fix} CORS에러

--- src/main/java/project/gaori/server/global/config/WebConfig.java
+++ src/main/java/project/gaori/server/global/config/WebConfig.java
@@ -1,7 +1,6 @@
 package project.gaori.server.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,17 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000," +
-                        "http://34.64.108.196:80," +
-                        "http://34.64.108.196")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.PATCH.name()
-                );
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
     }
 }
