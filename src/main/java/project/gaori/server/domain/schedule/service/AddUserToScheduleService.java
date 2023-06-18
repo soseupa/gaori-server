54e3cd6094c030f2ae626d7b6d1da235daa00fa3
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-18 19:15:15 +0900

    {Feat} 친구 스케쥴에 추가  알람 생성

--- src/main/java/project/gaori/server/domain/schedule/service/AddUserToScheduleService.java
+++ src/main/java/project/gaori/server/domain/schedule/service/AddUserToScheduleService.java
@@ -3,6 +3,7 @@ package project.gaori.server.domain.schedule.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.gaori.server.domain.notice.service.MakeInviteScheduleNoticeService;
 import project.gaori.server.domain.schedule.entity.Schedule;
 import project.gaori.server.domain.schedule.facade.ScheduleFacade;
 import project.gaori.server.domain.schedule.user.entity.ScheduleUser;
@@ -11,6 +12,8 @@ import project.gaori.server.domain.schedule.user.exception.ScheduleUserAlreadyEx
 import project.gaori.server.domain.user.entity.User;
 import project.gaori.server.domain.user.facade.UserFacade;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class AddUserToScheduleService {
@@ -18,14 +21,17 @@ public class AddUserToScheduleService {
     private final ScheduleFacade scheduleFacade;
     private final UserFacade userFacade;
     private final ScheduleUserRepository scheduleUserRepository;
+    private final MakeInviteScheduleNoticeService makeInviteScheduleNoticeService;
 
     @Transactional
-    public void execute(Long scheduleId, String email) {
+    public void execute(Long scheduleId, String email) throws IOException {
+        User me = userFacade.getCurrentUser();
         User user = userFacade.findUserByEmail(email);
         Schedule schedule = scheduleFacade.findScheduleById(scheduleId);
         this.scheduleUserRepository.findByUserAndSchedule(user, schedule).ifPresent(s -> {
             throw ScheduleUserAlreadyExistsException.EXCEPTION;
         });
         scheduleUserRepository.save(ScheduleUser.builder().schedule(schedule).user(user).build());
+        makeInviteScheduleNoticeService.execute(me, user, schedule);
     }
 }
--- src/main/java/project/gaori/server/domain/schedule/user/presentation/ScheduleUserController.java
+++ src/main/java/project/gaori/server/domain/schedule/user/presentation/ScheduleUserController.java
@@ -1,4 +1,4 @@
-package project.gaori.server.domain.schedule.user.entity.presentation;
+package project.gaori.server.domain.schedule.user.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.gaori.server.domain.schedule.service.AddUserToScheduleService;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/schedule/user")
 @RequiredArgsConstructor
@@ -18,7 +20,7 @@ public class ScheduleUserController {
 
     @Operation(summary = "일정에 친구 추가")
     @GetMapping("/add")
-    public void AddMember(@RequestParam Long id, @RequestParam String email) {
+    public void AddMember(@RequestParam Long id, @RequestParam String email) throws IOException {
         addUserToScheduleService.execute(id, email);
     }
 }
