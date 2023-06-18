02e847fc8c1f858fdd310d276bdaffd7c5ab9541
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-18 19:10:40 +0900

    {Feat} GetAccessTokenService

---
+++ src/main/java/project/gaori/server/domain/notice/service/GetAccessTokenService.java
@@ -0,0 +1,25 @@
+package project.gaori.server.domain.notice.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetAccessTokenService {
+
+    public String execute() throws IOException {
+        String firebaseConfigPath = "firebase/firebase_service_key.json";
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+}
