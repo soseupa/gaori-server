fa9d69300ef60a598fa3633f3c32d7a38a8d4e3d
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-19 09:40:42 +0900

    {Fix} RequestBody 필드 볁경

--- src/main/java/project/gaori/server/domain/notice/presentation/FcmController.java
+++ src/main/java/project/gaori/server/domain/notice/presentation/FcmController.java
@@ -1,8 +1,8 @@
 package project.gaori.server.domain.notice.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.gaori.server.domain.notice.service.CreateTargetTokenService;
@@ -14,8 +14,8 @@ public class FcmController {
     private final CreateTargetTokenService createTargetTokenService;
 
 
-    @PostMapping("/token")
-    public void updateToken(@RequestBody String targetToken) {
+    @PostMapping("/token/{targetToken}")
+    public void updateToken(@PathVariable String targetToken) {
         createTargetTokenService.execute(targetToken);
     }
 }
